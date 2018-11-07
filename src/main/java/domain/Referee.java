48d3b57a2374a239cc55a76a89364c5b8fb1e562
Regina <regy_escalera@hotmail.com>
2018-11-07 11:49:08 +0100

    delete reports and complaints
    delete reports and complaints

--- src/main/java/domain/Referee.java
+++ src/main/java/domain/Referee.java
@@ -1,37 +1,9 @@
 
 package domain;
 
-import java.util.Collection;
-
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.validation.Valid;
 
 @Entity
 public class Referee extends Actor {
-
-	private Collection<Report>		collectionReports;
-	private Collection<Complaint>	collectionComplaints;
-
-
-	@Valid
-	@OneToMany
-	public Collection<Report> getCollectionReports() {
-		return this.collectionReports;
-	}
-
-	public void setCollectionReports(final Collection<Report> collectionReports) {
-		this.collectionReports = collectionReports;
-	}
-
-	@Valid
-	@OneToMany
-	public Collection<Complaint> getCollectionComplaints() {
-		return this.collectionComplaints;
-	}
-
-	public void setCollectionComplaints(final Collection<Complaint> collectionComplaints) {
-		this.collectionComplaints = collectionComplaints;
-	}
 
 }
