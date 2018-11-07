e820702315ec372f735086c9d89f7f60d9ba2f82
Regina <regy_escalera@hotmail.com>
2018-11-07 11:48:40 +0100

    delete Report and @Temporal
    delete Report and @Temporal

--- src/main/java/domain/Note.java
+++ src/main/java/domain/Note.java
@@ -6,8 +6,8 @@ import java.util.Date;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
@@ -22,10 +22,9 @@ public class Note extends DomainEntity {
 	private String	customerComments;
 	private String	refereeComments;
 
-	private Report	report;
-
 
 	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
 	public Date getMoment() {
 		return this.moment;
 	}
@@ -65,16 +64,5 @@ public class Note extends DomainEntity {
 
 	public void setRefereeComments(final String refereeComments) {
 		this.refereeComments = refereeComments;
-	}
-
-	@Valid
-	@NotNull
-	@ManyToOne(optional = false)
-	public Report getReport() {
-		return this.report;
-	}
-
-	public void setReport(final Report report) {
-		this.report = report;
 	}
 }
